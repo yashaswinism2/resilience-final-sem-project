deef8e797c4708d3280e787e1ab26d6b0420747c
Yashaswini S M <yashusm2001@gmail.com>
2026-03-03 14:50:27 +0530

    backend main file

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from backend.app.api.question_routes import router as question_router
+# from backend.app.api.question_routes import router as question_router
+from app.api.question_routes import router as question_router
 import os
 
 # -------------------------------------------------
